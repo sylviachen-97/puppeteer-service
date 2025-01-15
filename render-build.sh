4e8f2df4bda08ae141dd892225ddb05dc1e5f626
Zixian Chen <zixian@Zixians-MacBook-Pro.local>
2025-01-15 14:38:14 +0100

    Second commit for Puppeteer service

---
+++ render-build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y wget gnupg
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y google-chrome-stable
