5941e4a46e99b36f8d13d10fb8014f84d621ae23
Zixian Chen <zixian@Zixians-MacBook-Pro.local>
2025-01-15 14:54:55 +0100

    Add Chrome installation verification to build script

--- render-build.sh
+++ render-build.sh
@@ -4,3 +4,6 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
 apt-get install -y google-chrome-stable
+echo "Verifying Chrome installation..."
+which google-chrome-stable || echo "Google Chrome not found!"
+google-chrome-stable --version || echo "Unable to retrieve Chrome version!"
